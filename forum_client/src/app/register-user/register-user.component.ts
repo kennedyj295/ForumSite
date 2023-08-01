dec69cc7cf79e9c350bf2188d363ed238cb5e03b
kennedyj295 <115056868+kennedyj295@users.noreply.github.com>
2023-08-01 17:57:59 -0500

    began work on register user form

--- forum_client/src/app/register-user/register-user.component.ts
+++ forum_client/src/app/register-user/register-user.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register-user',
   templateUrl: './register-user.component.html',
   styleUrls: ['./register-user.component.css']
 })
-export class RegisterUserComponent {
+export class RegisterUserComponent implements OnInit {
 
+  registerForm: FormGroup = new FormGroup({});
+  model: any = {};
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm() {
+    
+  }
+
+  register() {
+    
+  }
 }
